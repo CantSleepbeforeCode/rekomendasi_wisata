527f9072e678832cf12d6856e610884ceacdf848
Samsul Bahri <samsulbahrim25@gmail.com>
2024-06-02 21:13:20 +0800

    Update Resource

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,8 +19,8 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="/assets/images/logos/logo.png" width="180" alt="">
+                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100" style="padding: 0">
+                  <img src="/assets/images/logos/logo.png" width="" class="img-fluid" alt="">
                 </a>
                 <p class="text-center">Eroute Vacation Of Balikpapan</p>
                 <div class="col-md-12">
